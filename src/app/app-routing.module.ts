752afc8a6567add6325c966d41c8304a94bb5903
Your Name <you@example.com>
2021-07-08 22:10:54 +0200

    github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'about', component: AboutComponent},
+  { path: '', component: HomepageComponent},
+  { path: 'home', component: HomepageComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'albums', component: AlbumsComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { BlogComponent } from './blog/blog.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    ContactComponent,
+    LoginComponent,
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent
+  ], 
   imports: [
     BrowserModule,
     AppRoutingModule
